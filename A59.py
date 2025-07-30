036d25645c8ab7f91df54a2fbdc0986b2ba9f913
maehrm <maehrm@gmail.com>
2025-07-30 19:37:36 +0900

    Segment Tree added.

--- A59.py
+++ A59.py
@@ -10,14 +10,14 @@ class SegmentTree:
             self.tree[self.sz + i] = data[i]
         # 親ノードを構築(下から上へ)
         for i in range(self.sz - 1, 0, -1):
-            self.tree[i] = max(self.tree[2 * i], self.tree[2 * i + 1])
+            self.tree[i] = self.tree[2 * i] + self.tree[2 * i + 1]
 
     def update(self, i, x):
         i += self.sz
         self.tree[i] = x
         while i > 1:  # 葉から親へ辿る処理
             i //= 2
-            self.tree[i] = max(self.tree[i * 2], self.tree[i * 2 + 1])
+            self.tree[i] = self.tree[i * 2] + self.tree[i * 2 + 1]
 
     def query(self, l, r):
         l += self.sz
@@ -25,14 +25,15 @@ class SegmentTree:
         res = 0
         while l < r:
             if l % 2 == 1:
-                res = max(res, self.tree[l])
+                res += self.tree[l]
                 l += 1
             if r % 2 == 1:
                 r -= 1
-                res = max(res, self.tree[r])
+                res += self.tree[r]
             l //= 2
             r //= 2
         return res
+
 
 N, Q = map(int, input().split())
 A = [0] * N
