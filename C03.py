9a845e4a46eeb50922ca68c1f86f6ac402406708
maehrm <maehrm@gmail.com>
2025-08-23 17:45:20 +0900

    Stock Price.

---
+++ C03.py
@@ -0,0 +1,15 @@
+# 
+D = int(input())
+stock_price = [0] * D
+stock_price[0] = int(input())
+for i in range(D - 1):
+    stock_price[i + 1] = stock_price[i] + int(input())
+Q = int(input())
+for _ in range(Q):
+    s, t = map(lambda x: int(x) - 1, input().split())
+    if stock_price[s] > stock_price[t]:
+        print(s + 1)
+    elif stock_price[s] < stock_price[t]:
+        print(t + 1)
+    else:
+        print("Same")
